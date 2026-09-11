47b5ef209701cd3c3fab875b5bd02017b2b5f971
mariaceciliaalencar21-creator <mariaceciliaalencar21@gmail.com>
2026-09-11 09:50:33 -0300

    tipos-de-dados-undefined

---
+++ tipos-de-dados-undefined.js
@@ -0,0 +1,4 @@
+//Variável declarada mas sem valor inicializado
+let valorIndefinido;
+console.log("Valor indefinido:", valorIndefinido); // undefined
+console.log("Tipo de valorIndefinido:", typeof valorIndefinido); // "undefined"
