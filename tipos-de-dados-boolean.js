dac6291952e1bdb41208b66aa67c09c94a8d44d9
mariaceciliaalencar21-creator <mariaceciliaalencar21@gmail.com>
2026-09-11 09:19:47 -0300

    tipos-de-boolean

---
+++ tipos-de-dados-boolean.js
@@ -0,0 +1,6 @@
+let aprovado = true; // Valor booleano verdadeiro
+let reprovado = false; // Valor booleano falso
+console.log("Aprovado:", aprovado);
+console.log("Reprovado:", reprovado);
+console.log("Tipo de aprovado:", typeof aprovado);
+console.log("Tipo de reprovado:", typeof reprovado); 
