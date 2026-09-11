81fae31fd888a020665fe57a7aebacf0ce498d0a
mariaceciliaalencar21-creator <mariaceciliaalencar21@gmail.com>
2026-09-11 09:20:40 -0300

    tipos-de-dados-strings

---
+++ tipos-de-dados-strings.js
@@ -0,0 +1,6 @@
+let nome = "Ana"; // String com aspas duplas
+let sobrenome = 'Silva'; // String com aspas simples
+let saudacao = `Olá, meu nome é ${nome} ${sobrenome}!`; // Template literal com interpolação de variáveis
+console.log("Nome:", nome);
+console.log("Sobrenome:", sobrenome);
+console.log("Saudação:", saudacao);
