4ea5aa0105834df1d1c16dc971622fda13e5420d
mariaceciliaalencar21-creator <mariaceciliaalencar21@gmail.com>
2026-09-11 10:31:00 -0300

    tipos-de-dados-array

---
+++ tipos-de-dados-array.js
@@ -0,0 +1,6 @@
+let frutas = ["maçã", "banana", "laranja"]; // Array de strings
+let numeros = [1, 2, 3, 4, 5]; // Array de números
+let misturado = ["texto", 42, true, null]; // Array com tipos mistos
+console.log("Frutas:", frutas);
+console.log("Números:", numeros);
+console.log("Misturado:", misturado);
