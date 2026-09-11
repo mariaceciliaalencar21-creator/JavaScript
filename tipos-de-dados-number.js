19bd23e49bccfbd9f6f0ced90ad1953b6410591e
mariaceciliaalencar21-creator <mariaceciliaalencar21@gmail.com>
2026-09-11 09:20:19 -0300

    tipos-de-dados-number

---
+++ tipos-de-dados-number.js
@@ -0,0 +1,18 @@
+let idade = 25; // Número inteiro
+let altura = 1.75; // Número de ponto flutuante
+let temperatura = -5; // Número negativo
+console.log("Idade:", idade);
+console.log("Altura:", altura);
+console.log("Temperatura:", temperatura);   
+console.log("Tipo de idade:", typeof idade);
+console.log("Tipo de altura:", typeof altura);
+console.log("Tipo de temperatura:", typeof temperatura);
+console.log("Idade + Altura:", idade + altura);
+console.log("Idade - Temperatura:", idade - temperatura);
+console.log("Altura * Temperatura:", altura * temperatura);
+console.log("Idade / Altura:", idade / altura);
+console.log("Resto da divisão de Idade por Altura:", idade % altura);   
+console.log("Idade elevada à Altura:", idade ** altura);
+console.log("Idade arredondada para cima:", Math.ceil(idade));
+console.log("Idade arredondada para baixo:", Math.floor(idade));
+console.log("Idade arredondada para o inteiro mais próximo:", Math.round(idade));
